915fb7d49da8fc7fdcad02d9dbacc6cc31d9fec3
Rodion <1037886+brlo@users.noreply.github.com>
2022-07-26 00:51:40 +0300

    remember kwargs also

--- lib/sidekiq/delay_extensions/class_methods.rb
+++ lib/sidekiq/delay_extensions/class_methods.rb
@@ -16,8 +16,8 @@ module Sidekiq
       include Sidekiq::Worker
 
       def perform(yml)
-        (target, method_name, args) = YAML.load(yml)
-        target.__send__(method_name, *args)
+        (target, method_name, args, kwargs) = ::YAML.safe_load(yml, permitted_classes: [Symbol])
+        target.__send__(method_name, *args, **kwargs)
       end
     end
 
