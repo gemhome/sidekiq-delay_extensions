6cb1be414f4113727609f8846c1f528903c5a695
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2023-08-22 09:35:29 -0300

    Add safe_load for Sidekiq 7 compatibility

--- lib/sidekiq/delay_extensions/api.rb
+++ lib/sidekiq/delay_extensions/api.rb
@@ -34,6 +34,18 @@ module Sidekiq
                     super
         end
       end
+
+      private
+
+      def safe_load(content, default)
+        yield(*YAML.load(content))
+      rescue => ex
+        # #1761 in dev mode, it's possible to have jobs enqueued which haven't been loaded into
+        # memory yet so the YAML can't be loaded.
+        # TODO is this still necessary? Zeitwerk reloader should handle?
+        Sidekiq.logger.warn "Unable to load YAML: #{ex.message}" unless Sidekiq.options[:environment] == "development"
+        default
+      end
     end
   end
 end
